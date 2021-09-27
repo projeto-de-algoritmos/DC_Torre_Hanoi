a0f7e9e4638413b5443902196c1ef309913e0038
robertomatheus <as.matheusroberto@gmail.com>
2021-09-27 13:54:48 -0300

    Adicionando função para movimentar blocos para torres
    Signed-off-by: robertomatheus <as.matheusroberto@gmail.com>
    Co-authored-by: danieloda <danieloda35@gmail.com>

--- hanoi.js
+++ hanoi.js
@@ -28,6 +28,11 @@ class Rect {
         });
         $(document.body).append(this.div);
     }
+
+    moverPara(fn) {
+        var torre = this.torre;
+        return torre.moverRect(this, fn);
+    }
 }
 
 class Torre {
@@ -71,6 +76,33 @@ class Torre {
         this.div = div;
         $(document.body).append(div);
     }
+
+    moverRect(rect, fn) {
+        var podeMover = false;
+        if (this.rects.length === 0) {
+            podeMover = true;
+        } else {
+            var top = this.rects[this.rects.length -1];
+            if (top.indice > rect.indice) {
+                podeMover = true;
+            }
+        }
+        if (podeMover) {
+            if (rect.from) {
+                rect.from.rects = rect.from.rects.filter(function (item) {
+                    return item.indice !== rect.indice;
+                });
+            }
+            this.rects.push(rect);
+            rect.from = this;
+            var indice = this.rects.length;
+            rect.div.animate({
+                top : this.origem.y - rect.h * indice + "px",
+                left : this.origem.x + "px"
+            }, rect.velocidade, fn);
+        }
+        return podeMover;
+    }
 }
 
 function main(n, opt) {
@@ -85,6 +117,10 @@ function main(n, opt) {
     for (i = 0; i < TOTAL_TORRES; i++) {
         torres.push(new Torre(i));
     }
+
+    rects[0].to = torres[1];
+    rects[0].torre = torres[1];
+    rects[0].moverPara(()=>{return});
 }
 
 $( document ).ready(function() {
