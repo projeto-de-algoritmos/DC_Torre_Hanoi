49b18cb88f92d2e51c2c3ebf6672f2e1482aa497
robertomatheus <as.matheusroberto@gmail.com>
2021-09-27 13:30:56 -0300

    Adicionando visualização das torres
    Signed-off-by: robertomatheus <as.matheusroberto@gmail.com>
    Co-authored-by: danieloda <danieloda35@gmail.com>

--- hanoi.js
+++ hanoi.js
@@ -5,7 +5,6 @@ class Rect {
         this.passo = 15;
         this.h = 15;
         this.w = this.minimo + this.passo * this.indice;
-        this.div = null;
         this.from = null;
         this.torre = null;
         this.velocidade = opt.velocidade;
@@ -34,24 +33,57 @@ class Rect {
 class Torre {
     constructor(indice) {
         this.indice = indice;
-        this.h = 100;
+        this.h = 150;
         this.w = 50;
         this.top = 200;
-        this.left = 200;
+        this.left = 600;
         this.div = null;
-        this.origin = {
+        this.origem = {
+            y : this.top + this.h,
             x : this.left + this.indice * this.h + this.h,
-            y : this.top + this.h
         };
         this.rects = [];
+        this.draw();
+    }
+
+    draw() {
+        var div = document.createElement("div");
+        var div2 = document.createElement("div");
+        div = $(div);
+        div2 = $(div2);
+        div.css({
+            borderLeft : "1px solid #aaa",
+            height : this.h + "px",
+            width : 0,
+            position : "absolute",
+            left : this.origem.x + "px",
+            top : this.origem.y - this.h + "px"
+        });
+        div2.css({
+            position : "absolute",
+            width : this.w + "px",
+            height : 0,
+            borderBottom : "1px solid #aaa",
+            bottom : 0,
+            left : (- this.w / 2) + 'px'
+        });
+        div.append(div2);
+        this.div = div;
+        $(document.body).append(div);
     }
 }
 
 function main(n, opt) {
     var rects = [];
+    var torres = [];
+    const TOTAL_TORRES = 3;
 
     for (i = 0; i < n; i++) {
         rects.push(new Rect(i, {}));
+    }
+
+    for (i = 0; i < TOTAL_TORRES; i++) {
+        torres.push(new Torre(i));
     }
 }
 
