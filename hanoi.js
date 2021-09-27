c67b60ab77eb17edd41aff46de347052f1b0794e
robertomatheus <as.matheusroberto@gmail.com>
2021-09-26 23:54:22 -0300

    Adicionando classe Rect
    Signed-off-by: robertomatheus <as.matheusroberto@gmail.com>
    Co-authored-by: danieloda <danieloda35@gmail.com>

---
+++ hanoi.js
@@ -0,0 +1,11 @@
+class Rect {
+    constructor() {
+        this.h = 10;
+        this.w = this.minimo + this.passo * this.indice;
+        this.minimo = 20;
+        this.passo = 8;
+        this.div = null;
+        this.from = null;
+        this.torre = null;
+    }
+}
